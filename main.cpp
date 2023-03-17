df2a8af4889a2af8f26d4dbe8ae61ff4f7adb7ba
Program132 <clement.gauthier132@orange.fr>
2023-03-17 10:20:35 +0100

    Game main file.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 #include "src/GameInfo/GameInfo.h"
 #include "src/Vehicule/Vehicule.h"
-#include "src/Position/Position.h"
+#include "src/EndGame/EndGame.h"
 #include "src/GameManager/GameManager.h"
+#include "src/Move/Move.h"
 
 int main() {
     int size_x;
@@ -16,36 +17,42 @@ int main() {
     std::cin >> size_y;
     std::cout << "\n";
 
-    GameInfo game(size_x, size_y);
+    std::string v_name;
+    std::cout << "Name of your car: ";
+    std::cin >> v_name;
 
-    Vehicule vehicule("Ma superbe voiture");
-    Vehicule vehicule2("Ma superbe voiture", 3, 2);
-    Vehicule vehicule3("Ma superbe voiture", Position(3, 2));
-
-    std::cout << vehicule.toString() << std::endl;
-    std::cout << vehicule2.toString() << std::endl;
-    std::cout << vehicule3.toString() << std::endl;
-
-    vehicule.setX(5);
-    vehicule2.setPosition(10, 5);
-    vehicule3.setPosition(Position(10,20));
+    GameInfo gameInfo(size_x, size_y);
+    Vehicule vehicule(v_name, 2, size_y/2);
+    Move move;
+    EndGame endGame(gameInfo);
 
     std::cout << vehicule.toString() << std::endl;
-    std::cout << vehicule2.toString() << std::endl;
-    std::cout << vehicule3.toString() << std::endl;
 
     GameManager gameManager;
-    std::cout << gameManager.vehiculeIsAvailableInGame_toString(game, vehicule) << std::endl;
-    std::cout << gameManager.vehiculeIsAvailableInGame_toString(game, vehicule2) << std::endl;
-    std::cout << gameManager.vehiculeIsAvailableInGame_toString(game, vehicule3) << std::endl;
+    std::cout << gameManager.vehiculeIsAvailableInGame_toString(gameInfo, vehicule) << std::endl;
 
-    Vehicule vehiculeTest("Ma superbe voiture", 3, 2);
+    if (gameManager.vehiculeIsAvailableInGame(gameInfo, vehicule)) {
+        GameInfo::sendContentGame();
 
-    if (gameManager.vehiculeIsAvailableInGame(game, vehiculeTest)) {
-        gameManager.generateSpawnGame(game, vehiculeTest);
-        std::cout << "\n" << std::endl;
-        vehiculeTest.setPosition(0,0);
-        gameManager.generateSpawnGame(game, vehiculeTest);
+        while(!gameManager.GameOver) {
+            gameManager.generateSpawnGame(gameInfo, vehicule, endGame);
+            move.askAction();
+            move.toDown(gameManager, vehicule, gameInfo);
+            move.toUp(gameManager, vehicule, gameInfo);
+            move.toLeft(gameManager, vehicule, gameInfo);
+            move.toRight(gameManager, vehicule, gameInfo);
+            if (gameManager.isEndGame(endGame, vehicule)) {
+                gameManager.GameOver = true;
+                gameManager.GameWinner = vehicule.name;
+            }
+
+            for (int i = 0; i < 10; i++) {
+                std::cout << "\n";
+            }
+            std::cout << std::endl;
+        }
+        gameManager.generateSpawnGame(gameInfo, vehicule, endGame);
+        gameManager.endGame();
     }
 
     return 0;
