2bc0721014e109208292657d9f049aaf6f62393d
Program132 <clement.gauthier132@orange.fr>
2023-03-17 10:20:18 +0100

    Updated Game main class.

--- src/GameManager/GameManager.cpp
+++ src/GameManager/GameManager.cpp
@@ -1,20 +1,20 @@
 #include "GameManager.h"
 
-bool GameManager::vehiculeIsAvailableInGame(GameInfo gameInfo, Vehicule v) {
+bool GameManager::vehiculeIsAvailableInGame(GameInfo& gameInfo, Vehicule& v) {
     if (v.getX() <= gameInfo.getX() &&  v.getY() <= gameInfo.getY()) {
         return true;
     }
     return false;
 }
 
-std::string GameManager::vehiculeIsAvailableInGame_toString(GameInfo gameInfo, Vehicule v) {
+std::string GameManager::vehiculeIsAvailableInGame_toString(GameInfo& gameInfo, Vehicule& v) {
     if (v.getX() <= gameInfo.getX() &&  v.getY() <= gameInfo.getY()) {
         return "Yes";
     }
     return "No";
 }
 
-void GameManager::generateSpawnGame(GameInfo gameInfo, Vehicule v) {
+void GameManager::generateSpawnGame(GameInfo& gameInfo, Vehicule& v, EndGame& endGame) {
     /*
       x:0 1 2 3 4 5 6 7 8 9 -> 10 x | y
         - - - - - - - - - -           0
@@ -26,6 +26,8 @@ void GameManager::generateSpawnGame(GameInfo gameInfo, Vehicule v) {
         for (int j = 0; j < gameInfo.getX(); j++) {
             if (i == v.getY() && j == v.getX()) {
                 std::cout << "*";
+            } else if (i == endGame.position.p_y && j == endGame.position.p_x) {
+                std::cout << "A";
             } else {
                 std::cout << "-";
             }
@@ -33,4 +35,15 @@ void GameManager::generateSpawnGame(GameInfo gameInfo, Vehicule v) {
         std::cout << "\n";
     }
     std::cout << std::endl;
+}
+
+bool GameManager::isEndGame(EndGame &endGame, Vehicule &v) {
+    if (endGame.position.p_x == v.getX() && endGame.position.p_y == v.getY()) {
+        return true;
+    }
+    return false;
+}
+
+void GameManager::endGame() {
+    std::cout << "The winner is: " + this->GameWinner << "!" << std::endl;
 }
--- src/GameManager/GameManager.h
+++ src/GameManager/GameManager.h
@@ -5,13 +5,18 @@
 
 #include "../GameInfo/GameInfo.h"
 #include "../Vehicule/Vehicule.h"
+#include "../EndGame/EndGame.h"
 
 class GameManager {
 public:
-    bool vehiculeIsAvailableInGame(GameInfo gameInfo, Vehicule v);
-    std::string vehiculeIsAvailableInGame_toString(GameInfo gameInfo, Vehicule v);
+    bool GameOver;
+    std::string GameWinner;
 
-    void generateSpawnGame(GameInfo gameInfo, Vehicule v);
+    bool vehiculeIsAvailableInGame(GameInfo& gameInfo, Vehicule& v);
+    std::string vehiculeIsAvailableInGame_toString(GameInfo& gameInfo, Vehicule& v);
+    void generateSpawnGame(GameInfo& gameInfo, Vehicule& v, EndGame& endGame);
+    bool isEndGame(EndGame& endGame, Vehicule& v);
+    void endGame();
 };
 
 
